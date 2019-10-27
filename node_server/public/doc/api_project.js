b8af48a9fc81fb74152846b31e5eb5e494a2f918
lili <18354205371@163.com>
2019-10-27 15:21:02 +0800

    加入了微信小程序前台 引入有赞的ui   wepy真的不能用，腾讯公司个人开发维护的

---
+++ node_server/public/doc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "box-li",
+  "version": "0.1.0",
+  "description": "接口文档",
+  "title": "box-li接口文档",
+  "url": "http://localhost:3000",
+  "sampleUrl": "http://localhost:3000",
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-10-25T01:32:26.638Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
